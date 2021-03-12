afb84a0b25cacdf977cecbd19cca4dd94ca496c7
Antonio Loncar <anto-lon@github.com>
2021-03-12 23:30:37 +0100

    update
    rotate hamburger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const links = document.querySelectorAll('.nav-links li')
 
 hamburger.addEventListener("click", () => {
     navLink.classList.toggle("open");
+    hamburger.classList.toggle("rotate");
     links.forEach(link =>{
         link.classList.toggle("fade");
     });
